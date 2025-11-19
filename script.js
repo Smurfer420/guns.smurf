5ca8ed0951a34e1fd7ab853476835112ae30759d
smurfer420 <schlunsenloke@gmail.com>
2025-11-19 20:58:52 +0100

    fixed sound not working in sub pages

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function _copyText() {
-  const sound = new Audio("assets/ding.mp3");
-  sound.play();
+  // Resolve the audio path relative to the loaded script file so this works
+  // from subfolders (select-Page/, Staff-Experience/, etc.)
+  const scriptEl =
+    document.currentScript ||
+    (function () {
+      const scripts = document.getElementsByTagName("script");
+      return scripts[scripts.length - 1];
+    })();
+  const scriptSrc = scriptEl && scriptEl.src ? scriptEl.src : "";
+  const scriptBase = scriptSrc.replace(/script\.js(?:\?.*)?$/, "");
+  const audioPath = scriptBase
+    ? scriptBase + "assets/ding.mp3"
+    : "assets/ding.mp3";
+
+  const sound = new Audio(audioPath);
+  // play() can reject if the browser blocks it; ignore that error silently
+  sound.play().catch(() => {});
+
   navigator.clipboard.writeText("Smurfer420");
 
   const popup = document.getElementById("copy-popup");
-  popup.style.opacity = "1";
+  if (popup) {
+    popup.style.opacity = "1";
 
-  setTimeout(() => {
-    popup.style.opacity = "0";
-  }, 2500);
+    setTimeout(() => {
+      popup.style.opacity = "0";
+    }, 2500);
+  }
 }
